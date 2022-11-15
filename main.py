a4719e6ba06cd6d30ce47ebdde3ca3e0da82ad5a
mertsabinov <88425310+mertsabinov@users.noreply.github.com>
2022-11-15 16:40:57 +0200

    add: github command

--- main.py
+++ main.py
@@ -6,8 +6,12 @@ TOKEN = config["TOKEN"]
 
 bot = commands.Bot()
 
-@bot.slash_command(name="first_slash", guild_ids=[]) 
+@bot.slash_command(name="first_slash", guild_ids=[897544446088970260]) 
 async def first_slash(ctx): 
     await ctx.respond("You executed the slash command!")
+
+@bot.slash_command(name="github", guild_ids=[897544446088970260])
+async def github(ctx):
+    await ctx.respond("https://github.com/2STO-DE")
 
 bot.run(TOKEN)
